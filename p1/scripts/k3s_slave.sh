d5822c3538cd45f38ff1db94ea86399f3f575c39
Denis Chernyavskiy <denis.chernyavskiy@leroymerlin.ru>
2023-03-29 15:17:08 +0300

    Add part2 and correct Vagranfile

--- p1/scripts/k3s_slave.sh
+++ p1/scripts/k3s_slave.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-NAME="nenvoySW"
+export NAME="nenvoySW"
 echo "[INFO] Installing k3s on server worker node (ip: $2)"
 
-TOKEN_FILE="/vagrant/scripts/node-token"
+export TOKEN_FILE="/vagrant/scripts/node-token"
 echo "[INFO] Token: $(cat "$TOKEN_FILE")"
 
-INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file $TOKEN_FILE --node-ip=$2"
+export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file $TOKEN_FILE --node-ip=$2"
 echo "[INFO] ARGUMENT PASSED TO INSTALL_K3S_EXEC: $INSTALL_K3S_EXEC"
 
 apk add --no-cache curl
