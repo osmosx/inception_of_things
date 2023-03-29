a4567bd6be0e54e1c8f07e5fef1ccb6dd338d025
Denis Chernyavskiy <denis.chernyavskiy@leroymerlin.ru>
2023-03-29 12:47:46 +0300

    Add part1

--- p1/scripts/k3s_slave.sh
+++ p1/scripts/k3s_slave.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-export NAME="emaugaleSW"
-echo "[INFO]  Installing k3s on server worker node (ip: $2)"
+NAME="nenvoySW"
+echo "[INFO] Installing k3s on server worker node (ip: $2)"
 
-export TOKEN_FILE="/vagrant/scripts/node-token"
+TOKEN_FILE="/vagrant/scripts/node-token"
+echo "[INFO] Token: $(cat "$TOKEN_FILE")"
 
-echo "[INFO]  Token: $(cat $TOKEN_FILE)"
+INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file $TOKEN_FILE --node-ip=$2"
+echo "[INFO] ARGUMENT PASSED TO INSTALL_K3S_EXEC: $INSTALL_K3S_EXEC"
 
-export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file $TOKEN_FILE --node-ip=$2"
-
-echo "[INFO]  ARGUMENT PASSED TO INSTALL_K3S_EXEC: $INSTALL_K3S_EXEC"
-
-apk add curl
+apk add --no-cache curl
 
 curl -sfL https://get.k3s.io | sh -
 
-echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh # way to add alias on all users
+echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh # Adds an alias for kubectl to all users
+
