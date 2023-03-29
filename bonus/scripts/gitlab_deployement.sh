c2c75bb06670128b06584af267f3c5b0c4208792
Denis Chernyavskiy <denis.chernyavskiy@leroymerlin.ru>
2023-03-30 02:11:01 +0300

    Add bonus Gitlab

---
+++ bonus/scripts/gitlab_deployement.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export EMAIL="nenvoy@42.fr"
+export DOMAIN="gitlab.nenvoy.com"
+export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
+
+kubectl create namespace gitlab
+
+helm repo add gitlab https://charts.gitlab.io/
+
+helm search repo gitlab
+
+helm install gitlab gitlab/gitlab --set global.hosts.domain=$DOMAIN --set certmanager-issuer.email=$EMAIL --set global.hosts.https="false" --set global.ingress.configureCertmanager="false" --set gitlab-runner.install="false" -n gitlab
+
+echo -n "[INFO]   Gitlab password: "
+
+kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpath='{.data.password}' | base64 -d; echo
+
+echo 'Waiting for gitlab to be deployed'
+kubectl wait -n gitlab --for=condition=available deployment --all --timeout=-1s
+
+kubectl port-forward --address 0.0.0.0 svc/gitlab-webservice-default -n gitlab 8085:8181 | kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:443 > /var/log/argocd-port-forward.log 2>&1 &
+
